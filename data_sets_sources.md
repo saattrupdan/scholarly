da7a0818b147742d5afccbcd78da10b7250d570c
Dan Nielsen <saattrupdan@gmail.com>
2019-05-13 17:29:32 +0100

    compiled notebook

---
+++ data_sets_sources.md
@@ -0,0 +1,10 @@
+DATA SETS
+
+postcode_msoa.csv:
+https://geoportal.statistics.gov.uk/datasets/postcode-to-output-area-to-lower-layer-super-output-area-to-middle-layer-super-output-area-to-local-authority-district-february-2018-lookup-in-the-uk/data
+
+msoa_income.xls:
+https://www.ons.gov.uk/employmentandlabourmarket/peopleinwork/earningsandworkinghours/datasets/smallareaincomeestimatesformiddlelayersuperoutputareasenglandandwales
+
+house_prices.csv
+https://www.gov.uk/government/statistical-data-sets/price-paid-data-downloads
--- extract_elmo.py
+++ extract_elmo.py
@@ -9,47 +9,62 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' # ignore tensorflow warnings
 import tensorflow_hub as hub
 import tensorflow as tf
 
-def elmo_vectors(x, data_rows = 0):
+def elmo_vectors(x, sess, current_batch=0, num_batches=0):
     
     # the actual ELMo feature extraction
-    embeddings = elmo_model(x.tolist(), signature="default", as_dict=True)["elmo"]
+    embeddings = elmo_model(
+            x.tolist(),
+            signature="default", 
+            as_dict=True)["elmo"]
     
     # report progress
-    index = x.keys().tolist()[-1]
-    if data_rows:
-        print(f"Extracting ELMo features from {file_name}.csv... " \
-                f"{round(index / data_rows * 100, 2)}% completed.", end = "\r")
+    if num_batches and current_batch:
+        print(f"Extracting ELMo features from the {data_rows} rows " \
+                f"in {file_name}_clean_text.pickle... " \
+                f"{round(current_batch / num_batches * 100, 2)}% " \
+                f"completed.", end = "\r")
 
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
-        sess.run(tf.tables_initializer())
-
-        # return average of ELMo features
-        return sess.run(tf.reduce_mean(embeddings,1))
+    # return average of ELMo features
+    #return sess.run(embeddings)
+    return sess.run(tf.reduce_mean(embeddings, 1))
 
 
 file_name = f'{argv[1]}'
 
 # set up dataframe with cleaned text
-with open(f"{file_name}_clean_text.pickle", "rb") as pickle_in:
+with open(f"{file_name}_lemmatised.pickle", "rb") as pickle_in:
     df = pickle.load(pickle_in)
-
-# get the amount of rows in the dataframe
-data_rows = df.shape[0]
 
 # load the ELMo model
 elmo_model = hub.Module("elmo", trainable=False)
 
-print(f"Extracting ELMo features from {file_name}.csv...", end = "\r")
+# get the amount of rows in the data set
+data_rows = len(df['clean_text'])
+
+print(f"Extracting ELMo features from the {data_rows} rows " \
+        f"in {file_name}_lemmatised.pickle...", end = "\r")
+
+# set up batches, larger batch size requires more computational resources
+# but yields more accurate ELMo vectors
+batch_size = 50
+batches = np.asarray(
+        [df[i:i+batch_size]['clean_text'] for i in 
+            np.arange(0, data_rows, batch_size)]
+        )
+num_batches = len(batches)
 
 # build ELMo data
-batch_size = 25
-batches = np.asarray([df[i:i+batch_size]['clean_text'] for i in range(0, data_rows, batch_size)])
-elmo_batches = np.asarray([elmo_vectors(batch, data_rows) for batch in batches])
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    sess.run(tf.tables_initializer())
+    elmo_batches = np.asarray([elmo_vectors(batch, sess, i, num_batches)
+            for i, batch in enumerate(batches)])
+    
 elmo_data = np.concatenate(elmo_batches, axis = 0)
 
 # save ELMo data
-with open(f"{file_name}_elmo.pickle","wb") as pickle_out:
+with open(f"{file_name}_elmo_lemmatised.pickle","wb") as pickle_out:
     pickle.dump(elmo_data, pickle_out)
 
-print("All done! The ELMo data is saved as {file_name}_elmo.pickle.")
+print(' ' * 100, end = '\r')
+print(f"All done! The ELMo data is saved as {file_name}_elmo.pickle.")
