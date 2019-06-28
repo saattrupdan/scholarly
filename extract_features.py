a02bec0825166d7f1c5b40172ebd4e52387ea926
Dan Saattrup Nielsen <saattrupdan@gmail.com>
2019-06-28 15:19:19 +0100

    added doomsday clock to preserve same level of memory usage over long periods of time

--- extract_features.py
+++ extract_features.py
@@ -1,12 +1,12 @@
-from sys import argv
-import os
-import pickle
 import cleaner
 import elmo
+import os
+import sys
+import itertools as it
 
 # set list of file_names
-if len(argv) > 1:
-    file_names = argv[1:]
+if len(sys.argv) > 1:
+    file_names = sys.argv[1:]
 else:
     file_names = [f'arxiv_sample_{i}' for i in
         [1000, 5000, 10000, 25000, 50000, 100000, 200000, 500000, 750000]] + ['arxiv']
@@ -20,18 +20,20 @@ elmo.download_elmo_model()
 for file_name in file_names:
     print("------------------------------------")
     print(f"NOW PROCESSING: {file_name}")
-    print("--------------------------")
+    print("------------------------------------")
     output_path = os.path.join(data_path, f"{file_name}_elmo.csv")
     if not os.path.isfile(output_path):
         cleaner.clean(
             file_name = file_name, 
-            lemm_batch_size = 100, 
+            lemm_batch_size = 500, 
             path = data_path
             )
         elmo.extract(
             file_name = file_name,
             path = data_path,
-            batch_size = 10
+            batch_size = 10,
+            countdown = True,
+            confirmation = True
             )
     else:
         print(f"Already ELMo'd that one. Moving on...")
