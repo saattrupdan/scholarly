be9f35840910423431fc934ff588b8efea3db566
Dan Nielsen <saattrupdan@gmail.com>
2019-06-28 07:54:26 +0100

    small additions

--- extract_features.py
+++ extract_features.py
@@ -8,8 +8,8 @@ import elmo
 if len(argv) > 1:
     file_names = argv[1:]
 else:
-    file_names = (f'arxiv_sample_{i}' for i in
-        [1000, 5000, 10000, 25000, 50000, 100000, 200000, 500000, 750000])
+    file_names = [f'arxiv_sample_{i}' for i in
+        [1000, 5000, 10000, 25000, 50000, 100000, 200000, 500000, 750000]] + ['arxiv']
 
 #data_path = os.path.join("P:/", "Public Folder", "scholarly_data")
 data_path = os.path.join("/home", "leidem", "pCloudDrive", "Public Folder", "scholarly_data")
@@ -22,11 +22,15 @@ for file_name in file_names:
     print("--------------------------")
     output_path = os.path.join(data_path, f"{file_name}_elmo.csv")
     if not os.path.isfile(output_path):
-        cleaner.clean(file_name, lemm_batch_size = 100, path = data_path)
-        elmo.extract(
-            file_name = file_name,
-            path = data_path,
-            batch_size = 10
-        )
+        cleaner.clean(
+            file_name = file_name, 
+            lemm_batch_size = 100, 
+            path = data_path
+            )
+        #elmo.extract(
+        #    file_name = file_name,
+        #    path = data_path,
+        #    batch_size = 10
+        #    )
     else:
         print(f"Already ELMo'd that one. Moving on...")
