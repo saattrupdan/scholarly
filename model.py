d74068dd5208a532e76b551821de47e58103b548
Dan Saattrup Nielsen <saattrupdan@gmail.com>
2019-12-15 17:08:52 +0000

    feat: Add random forest

--- model.py
+++ model.py
@@ -3,19 +3,27 @@ from torch import nn
 from torch import optim
 from torch.nn import functional as F
 
-class BaseMLP(nn.Module):
-    def __init__(self, dim, vocab_size, emb_dim, emb_matrix):
+class Base(nn.Module):
+    def trainable_params(self):
+        train_params = (param in self.parameters() if param.requires_grad)
+        return sum(param.numel() for param in train_params)
+
+class Embedding(Base):
+    def __init__(self, vocab_size, emb_dim, emb_matrix):
         super().__init__()
         self.embed = nn.Embedding(vocab_size, emb_dim)
         self.embed.weight.data.copy_(emb_matrix)
         self.embed.weight.requires_grad = False
+
+    def forward(self, x):
+        return self.embed(x)
+
+class MLP(Base):
+    def __init__(self, dim, vocab_size, emb_dim, emb_matrix):
+        super().__init__()
+        self.embed = Embedding(vocab_size, emb_dim, emb_matrix)
         self.fc = nn.Linear(emb_dim, dim)
         self.out = nn.Linear(dim, 6)
-
-    def trainable_params(self):
-        ''' Get the number of trainable parameters in the model. '''
-        return sum(param.numel() for param in self.parameters() 
-                if param.requires_grad)
 
     def forward(self, x):
         x = self.embed(x)
@@ -23,25 +31,65 @@ class BaseMLP(nn.Module):
         x = F.elu(self.fc(x))
         return torch.sigmoid(self.out(x)).squeeze()
 
-def train(model, train_dl, val_dl, epochs = 10, lr = 3e-4):
+class RandomForest(Base):
+    def __init__(self, vocab_size, emb_dim, emb_matrix, trees_per_batch = 100):
+        super().__init__()
+        from sklearn.ensemble import RandomForestClassifier
+        self.embed = Embedding(vocab_size, emb_dim, emb_matrix)
+        self.trees_per_batch = trees_per_batch
+        self.forest = RandomForestClassifier(
+            n_estimators = self.trees_per_batch
+        )
+
+    @staticmethod
+    def combine_forests(forest1, forest2):
+        forest1.estimators_ += forest2.estimators_
+        forest1.n_estimators = len(forest1.estimators_)
+        return forest1
+    
+    def fit(self, train_dl):
+        from sklearn.ensemble import RandomForestClassifier
+        from functools import reduce
+        forests = []
+        for x_train, y_train in train_dl:
+            rf = RandomForestClassifier(n_estimators = self.trees_per_batch)
+            rf.fit(x_train, y_train)
+            forests.append(rf)
+        self.forest = reduce(combine_forests, forests)
+        return self
+
+    def score(self, val_dl):
+        from sklearn.metrics import f1_score
+        f1s = []
+        for x_val, y_val in val_dl:
+            f1s.append(f1_score(y_val, y_hat, average = 'samples'))
+        return sum(f1s) / len(f1s)
+
+    def predict(self, x):
+        return torch.LongTensor(self.forest.predict(x))
+
+    def predict_proba(self, x):
+        return torch.FloatTensor(self.forest.predict_proba(x))
+
+def train_nn(nn, train_dl, val_dl, epochs = 10, lr = 3e-4):
     from tqdm.auto import tqdm
 
     print(f'Training on {len(train_dl) * train_dl.batch_size:,d} samples '\
           f'and validating on {len(val_dl) * val_dl.batch_size:,d} samples.')
-    print(f'Number of trainable parameters: {model.trainable_params():,d}')
+    print(f'Number of trainable parameters: {nn.trainable_params():,d}')
 
-    optimizer = optim.Adam(model.parameters(), lr = lr)
+    optimizer = optim.Adam(nn.parameters(), lr = lr)
     criterion = nn.BCELoss()
 
     for epoch in range(epochs):
         with tqdm(total = len(train_dl) * train_dl.batch_size) as pbar:
-            model.train()
+            nn.train()
 
             tot_loss, avg_loss = 0, 0
             for idx, (x_train, y_train) in enumerate(train_dl):
                 optimizer.zero_grad()
 
-                y_hat = model.forward(x_train)
+                y_hat = nn.forward(x_train)
                 loss = criterion(y_hat, y_train)
                 loss.backward()
                 optimizer.step()
@@ -53,10 +101,10 @@ def train(model, train_dl, val_dl, epochs = 10, lr = 3e-4):
                 pbar.update(train_dl.batch_size)
 
             with torch.no_grad():
-                model.eval()
+                nn.eval()
                 val_loss = 0
                 for x_val, y_val in val_dl:
-                    y_hat = model.forward(x_val)
+                    y_hat = nn.forward(x_val)
                     val_loss += criterion(y_hat, y_val)
                 val_loss /= len(val_dl)
 
@@ -66,6 +114,16 @@ def train(model, train_dl, val_dl, epochs = 10, lr = 3e-4):
 
 if __name__ == '__main__':
     from data import load_data
-    train_dl, val_dl, params = load_data('arxiv_data_mcats_pp')
-    model = BaseMLP(dim = 500, **params)
-    train(model, train_dl, val_dl, epochs = 100, lr = 3e-4)
+
+    train_dl, val_dl, params = load_data(
+        'arxiv_data_mcats_pp',
+        vectors = 'fasttext',
+        batch_size = 10000
+    )
+
+    rf = RandomForest(trees_per_batch = 100, **params)
+    rf.fit(train_dl)
+    print(rf.score(val_dl))
+
+    #mlp = BaseMLP(dim = 500, **params)
+    #train_nn(mlp, train_dl, val_dl, epochs = 50, lr = 3e-4)
