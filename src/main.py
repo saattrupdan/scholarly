a43ac1ea8eb1d3f8327b44674b3b34cb5d6aa14d
Dan Nielsen <saattrupdan@gmail.com>
2020-01-04 23:42:28 +0100

    feat: Add argument parser

--- src/main.py
+++ src/main.py
@@ -1,8 +1,6 @@
-def main(mcat_ratio):
+def main(mcat_ratio: float = 0.5, epochs: int = 5, dim: int = 128, 
+    model: str = 'sharnn', nlayers: int = 1) -> str:
     from data import load_data
-    from modules import SHARNN, load_model
-    from training import train_model
-    from inference import get_scores
 
     train_dl, val_dl, params = load_data(
         tsv_fname = 'arxiv_data_cats_pp_mini',
@@ -11,16 +9,58 @@ def main(mcat_ratio):
         split_ratio = 0.9
     )
 
-    model = SHARNN(dim = 128, boom_dim = 512, **params)
-    model = train_model(model, train_dl, val_dl, 
-        epochs = 3, 
+    if model == 'logreg':
+        from modules import LogisticRegression
+        model = LogisticRegression
+    elif model == 'mlp':
+        from modules import MLP
+        model = MLP
+    elif model == 'cnn':
+        from modules import CNN
+        model = CNN
+    elif model == 'sharnn':
+        from modules import SHARNN
+        model = SHARNN
+    else:
+        raise RuntimeError('Invalid model, please choose between '\
+                           '"logreg", "mlp", "cnn" and "sharnn".')
+
+    print(f'Training {model.__name__} with dimension {dim} and {nlayers} '\
+          f'layer(s), for {epochs} epoch(s) with mcat ratio {mcat_ratio}.')
+
+    model = model(dim = dim, nlayers = nlayers, **params)
+    model = model.fit(train_dl, val_dl, 
+        epochs = epochs, 
         lr = 3e-4,
         mcat_ratio = mcat_ratio
     )
 
-    return get_scores(model, val_dl)
+    return model.evaluate(val_dl)
 
 if __name__ == '__main__':
-    print(main(0.0))
-    print(main(0.5))
-    print(main(1.0))
+    from argparse import ArgumentParser
+    import numpy as np
+
+    ap = ArgumentParser()
+    ap.add_argument('-l', '--layers', type = int, default = [1], nargs = '*')
+    ap.add_argument('-e', '--epochs', type = int, default = [5], nargs = '*')
+    ap.add_argument('-d', '--dim', type = int, default = [128], nargs = '*')
+    ap.add_argument('-m', '--model', default = 'sharnn', nargs = '*',
+        type = str, choices = ['sharnn', 'logreg', 'cnn', 'mlp'])
+    ap.add_argument('-r', '--ratio', type = float, nargs = '+', 
+        choices = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
+    args = vars(ap.parse_args())
+
+    for model in args['model']:
+        for ratio in args['ratio']:
+            for epochs in args['epochs']:
+                for dim in args['dim']:
+                    for layers in args['layers']:
+                        scores = main(
+                            model = model,
+                            mcat_ratio = ratio,
+                            epochs = epochs,
+                            dim = dim,
+                            nlayers = layers
+                        )
+                        print(scores)
