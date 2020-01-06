1c9295047e0c072ac48e32c7dc6c21312a395bbb
Dan Saattrup Nielsen <saattrupdan@gmail.com>
2020-01-06 14:44:12 +0000

    feat: Only one data_dir

--- src/db.py
+++ src/db.py
@@ -13,6 +13,7 @@ class ArXivDatabase:
         from utils import get_path
         db_path = get_path(data_dir) / name
         self.engine = create_engine(f'sqlite:///{db_path}')
+        self.data_dir = data_dir
         self.create_tables()
         self.populate_cats()
 
@@ -182,7 +183,7 @@ class ArXivDatabase:
 
         return self
 
-    def get_cats(self, conn = None, data_dir: str = '.data') -> list:
+    def get_cats(self, conn = None) -> list:
         ''' Get a list of all the categories. '''
         from utils import get_path
         import json
@@ -195,11 +196,12 @@ class ArXivDatabase:
             cat_result = conn.execute('select id from cats')
             cats = [cat[0] for cat in cat_result]
 
-        if save_to is not None:
-            with open(get_path(data_dir) / 'cats.json', 'w') as f:
-                json.dump(cats, f)
+        with open(get_path(self.data_dir) / 'cats.json', 'w') as f:
+            json.dump(cats, f)
 
-    def get_mcat_dict(self, conn = None, data_dir: str = '.data') -> dict:
+        return cats
+
+    def get_mcat_dict(self, conn = None) -> dict:
         ''' Get a dictionary mapping each category to its master category. '''
         from utils import get_path
         import json
@@ -212,20 +214,14 @@ class ArXivDatabase:
             mcat_result = conn.execute('select id, master_cat from cats')
             mcat_dict = {pair[0]: pair[1] for pair in mcat_result}
 
-        if save_to is not None:
-            with open(get_path(data_dir) / 'mcat_dict.json', 'w') as f:
-                json.dump(mcat_dict, f)
+        with open(get_path(self.data_dir) / 'mcat_dict.json', 'w') as f:
+            json.dump(mcat_dict, f)
 
         return mcat_dict
 
-    def get_training_df(self, only_master_cats: bool = False):
+    def get_training_df(self):
         ''' Get a dataframe with titles, abstracts and categories
             of all the papers in the database.
-
-        INPUT
-            only_master_cats: bool = False
-                Whether to have columns for every master category, or to
-                have columns for every category
 
         OUTPUT
             A Pandas DataFrame object with columns title, abstract (or a
@@ -234,11 +230,11 @@ class ArXivDatabase:
         '''
         import pandas as pd
         from tqdm.auto import tqdm
+        from utils import get_path
 
         with self.engine.connect() as conn:
 
             cats = self.get_cats(conn)
-            mcat_dict = self.get_mcat_dict(conn)
 
             # Convert the papers table in the database to a dataframe
             df = pd.read_sql_table(
@@ -256,30 +252,18 @@ class ArXivDatabase:
                 paper_ids = [paper[0] for paper in conn.execute(query)]
 
                 bool_col = df['id'].isin(paper_ids).astype(int)
-                if only_master_cats:
-                    mcat = mcat_dict[cat]
-                    if mcat in df.columns:
-                        df[mcat] = (df[mcat] | bool_col).astype(int)
-                    else:
-                        df[mcat] = bool_col
-                else:
-                    df[cat] = bool_col
+                df[cat] = bool_col
 
-        return df.drop(columns = ['id'])
+        df = df.drop(columns = ['id'])
+        df.to_csv(get_path(self.data_dir) / 'arxiv_data.tsv', sep = '\t',
+            index = False)
+        return df
 
 if __name__ == '__main__':
-    from utils import get_path
-
-    # Open database
     db = ArXivDatabase(data_dir = '.data')
-
-    db.get_mcat_dict(save_to = get_path('.data') / 'mcat_dict.json')
-    db.get_cats(save_to = get_path('.data') / 'cats.json')
-
-    # Pull out a abstracts and categories into a tsv file
-    #df = db.get_training_df(only_master_cats = True)
-    #df.to_csv(get_path('.data') / 'arxiv_data_mcats.tsv', sep = '\t', 
-    #    index = False)
+    db.get_mcat_dict()
+    db.get_cats()
+    db.get_training_df()
 
     # Example query: Output the number of authors in database
     #with db.engine.connect() as conn:
