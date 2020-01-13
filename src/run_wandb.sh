568053c15e0d6717324aab328cf653392d200479
Dan Saattrup Nielsen <saattrupdan@gmail.com>
2020-01-13 18:39:00 +0000

    feat: Add script to perform multiple wandb runs

---
+++ src/run_wandb.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+python main.py --epochs 50 --fname arxiv_data_test --vectors glove --name vector_experiment
+python main.py --epochs 50 --fname arxiv_data_test --vectors glove --name vector_experiment
+python main.py --epochs 50 --fname arxiv_data_test --vectors glove --name vector_experiment
+
+python main.py --epochs 50 --fname arxiv_data_test --vectors fasttext --name vector_experiment
+python main.py --epochs 50 --fname arxiv_data_test --vectors fasttext --name vector_experiment
+python main.py --epochs 50 --fname arxiv_data_test --vectors fasttext --name vector_experiment
+python main.py --epochs 50 --fname arxiv_data_test --vectors glove --name vector_experiment
+python main.py --epochs 50 --fname arxiv_data_test --vectors glove --name vector_experiment
+python main.py --epochs 50 --fname arxiv_data_test --vectors glove --name vector_experiment
+
+python main.py --epochs 50 --fname arxiv_data_test --vectors fasttext --name vector_experiment
+python main.py --epochs 50 --fname arxiv_data_test --vectors fasttext --name vector_experiment
+python main.py --epochs 50 --fname arxiv_data_test --vectors fasttext --name vector_experiment
+python main.py --epochs 50 --fname arxiv_data_test --vectors glove --name vector_experiment
+python main.py --epochs 50 --fname arxiv_data_test --vectors glove --name vector_experiment
+python main.py --epochs 50 --fname arxiv_data_test --vectors glove --name vector_experiment
