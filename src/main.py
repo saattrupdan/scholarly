04f728f7d62e86c4560096924245f54abf64f6c1
Dan Saattrup Nielsen <saattrupdan@gmail.com>
2020-01-17 18:45:09 +0000

    feat: Added some more parameters

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
-def main(mcat_ratio: float, epochs: int, dim: int, nlayers: int, fname: str, 
-    gpu: bool, name: str, lr: float, batch_size: int, split_ratio: float, 
-    vectors: str, data_dir: str, pbar_width: int, wandb: bool, boom_dim: int,
-    dropout: float) -> str:
+def main(mcat_factor: float, epochs: int, dim: int, 
+    nlayers: int, fname: str, gpu: bool, name: str, lr: float, 
+    batch_size: int, split_ratio: float, vectors: str, data_dir: str, 
+    pbar_width: int, wandb: bool, boom_dim: int, dropout: float, 
+    ema: float) -> str:
     from data import load_data
     from utils import get_path
+    from modules import SHARNN
 
     pp_path = get_path(data_dir) / f'{fname}_pp.tsv'
     if not pp_path.is_file():
@@ -39,9 +41,10 @@ def main(mcat_ratio: float, epochs: int, dim: int, nlayers: int, fname: str,
     model = model.fit(train_dl, val_dl, 
         epochs = epochs, 
         lr = lr,
-        mcat_ratio = mcat_ratio,
+        mcat_factor = mcat_factor,
         name = name,
-        use_wandb = wandb
+        use_wandb = wandb,
+        ema = ema
     )
 
     return model.evaluate(val_dl)
@@ -53,19 +56,20 @@ if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('--name', default = 'no_name')
     parser.add_argument('--batch_size', type = int, default = 32)
-    parser.add_argument('--split_ratio', type = float, default = 0.95)
+    parser.add_argument('--split_ratio', type = float, default = 0.9)
     parser.add_argument('--lr', type = float, default = 3e-4)
     parser.add_argument('--nlayers', type = int, default = 1)
     parser.add_argument('--epochs', type = int, default = 5)
     parser.add_argument('--dim', type = int, default = 256)
     parser.add_argument('--boom_dim', type = int, default = 512)
-    parser.add_argument('--mcat_ratio', type = float,  default = 0.1)
+    parser.add_argument('--mcat_factor', type = float, default = 0.85)
     parser.add_argument('--fname', default = 'arxiv_data')
     parser.add_argument('--gpu', type = boolean, default = False)
     parser.add_argument('--wandb', type = boolean, default = True)
     parser.add_argument('--data_dir', default = '.data')
     parser.add_argument('--pbar_width', type = int, default = None)
     parser.add_argument('--dropout', type = float, default = 0.)
+    parser.add_argument('--ema', type = float, default = 0.99)
     parser.add_argument('--vectors', choices = ['fasttext', 'glove'],
         default = 'fasttext')
 
