e029291c6710b37b031fcec0f604f2bd13b2f11b
Dan Saattrup Nielsen <saattrupdan@gmail.com>
2020-01-17 18:45:34 +0000

    test: Factor test

--- src/run_wandb.sh
+++ src/run_wandb.sh
@@ -1,15 +1,4 @@
 #!/bin/sh
-python main.py --epochs 50 --fname arxiv_data_test --vectors fasttext --name vector_experiment
-python main.py --epochs 50 --fname arxiv_data_test --vectors fasttext --name vector_experiment
-python main.py --epochs 50 --fname arxiv_data_test --vectors fasttext --name vector_experiment
-python main.py --epochs 50 --fname arxiv_data_test --vectors fasttext --name vector_experiment
-python main.py --epochs 50 --fname arxiv_data_test --vectors fasttext --name vector_experiment
-python main.py --epochs 50 --fname arxiv_data_test --vectors fasttext --name vector_experiment
-
-python main.py --epochs 50 --fname arxiv_data_test --vectors glove --name vector_experiment
-python main.py --epochs 50 --fname arxiv_data_test --vectors glove --name vector_experiment
-python main.py --epochs 50 --fname arxiv_data_test --vectors glove --name vector_experiment
-python main.py --epochs 50 --fname arxiv_data_test --vectors glove --name vector_experiment
-python main.py --epochs 50 --fname arxiv_data_test --vectors glove --name vector_experiment
-python main.py --epochs 50 --fname arxiv_data_test --vectors glove --name vector_experiment
-python main.py --epochs 50 --fname arxiv_data_test --vectors glove --name vector_experiment
+python main.py --epochs 100 --fname arxiv_data_test --mcat_min 0.1 --mcat_factor 0.9 --name factor_test
+python main.py --epochs 100 --fname arxiv_data_test --mcat_min 0.1 --mcat_factor 0.8 --name factor_test
+python main.py --epochs 100 --fname arxiv_data_test --mcat_min 0.1 --mcat_factor 0.7 --name factor_test
