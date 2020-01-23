2f4efb02821a4d2e1ecb0a54603f8433e7cf17c5
Dan Saattrup Nielsen <saattrupdan@gmail.com>
2020-01-23 12:13:37 +0000

    chore: Rename shell script, run simple models

--- src/run_wandb.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-python main.py --epochs 100 --fname arxiv_data_test --mcat_min 0.1 --mcat_factor 0.9 --name factor_test
-python main.py --epochs 100 --fname arxiv_data_test --mcat_min 0.1 --mcat_factor 0.8 --name factor_test
-python main.py --epochs 100 --fname arxiv_data_test --mcat_min 0.1 --mcat_factor 0.7 --name factor_test
